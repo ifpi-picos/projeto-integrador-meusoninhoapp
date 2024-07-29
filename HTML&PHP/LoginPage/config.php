f0da3830fbd80ab61aee8313cebfe17785e8aa7a
Antônio Enzo Bezerra / An1Toh <159924839+antonioenzobz@users.noreply.github.com>
2024-07-28 23:49:18 -0300

    Add files via upload

---
+++ HTML&PHP/LoginPage/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$DBhost = "localhost";
+$DBusuario = "root";
+$DBsenha = "9f@Y4L#vQ1";
+$DBnome = "DataBase-MeuSoninhoAPP";
+
+$DBconexao = new mysqli($DBhost, $DBusuario, $DBsenha, $DBnome);
+
+?>
